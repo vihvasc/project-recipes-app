b67891b971f9d5971caddd3e82d251124138cb68
alvesgf16 <alvesgabriel1994@gmail.com>
2021-12-09 16:15:20 -0300

    Adiciona rotas ao App

---
+++ src/pages/ExploreDrinkIngredients.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ExploreDrinkIngredients() {
+  return (
+    <h1>Explorar bebidas por ingrediente</h1>
+  );
+}
+
+export default ExploreDrinkIngredients;
---
+++ src/pages/ExploreFoodOrigin.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ExploreFoodOrigin() {
+  return (
+    <h1>Explorar comidas por local de origem</h1>
+  );
+}
+
+export default ExploreFoodOrigin;
