8372238f522b1eed2c36c6c26cae7bd79c8e1b28
1lucascq <1lucascq@gmail.com>
2021-12-10 12:16:38 -0300

    ajustes de lint

--- src/pages/FoodPages/FoodRecipeDetails.js
+++ src/pages/FoodPages/FoodRecipeDetails.js
@@ -5,10 +5,11 @@ import AppContext from '../../context/AppContext';
 // Cada card deve conter o data-testid="${index}-recipe-card".
 // Cada imagem deve conter o data-testid="${index}-card-img".
 // Cada tag com o nome da receita deve ter o data-testid="${index}-card-name".
-// Caso mais de uma bebida seja encontrada, mostrar as 12 primeiras (ou menos, se não hoverem 12).
+// Caso mais de uma bebida seja encontrada, mostrar as 12 primeiras (ou menos, se não houverem 12).
 
 export default function FoodRecipeDetails() {
   const { id } = useParams();
+  console.log(id);
   const { data } = useContext(AppContext);
   console.log('data ao renderizar o :', data);
   return (
