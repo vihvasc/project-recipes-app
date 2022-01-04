7a896cbe1ba334f8240881e75694806199518fa2
Virginia <vihvasc@gmail.com>
2022-01-04 15:16:53 -0300

    Pagina de perfil

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,14 +1,54 @@
 import React from 'react';
-import Header from '../components/Header';
+import { useHistory } from 'react-router';
 import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 function Profile() {
+  const { email } = JSON.parse(localStorage.getItem('user'));
+  const history = useHistory();
+  function recipesDone() {
+    history.push('/receitas-feitas');
+  }
+
+  function recipesFavorites() {
+    history.push('/receitas-favoritas');
+  }
+
+  function LogoutButton() {
+    localStorage.clear();
+    history.push('/');
+  }
+
   return (
     <div>
+      <Header pageTitle="Perfil" />
+      <h2 data-testid="profile-email">
+        {email}
+      </h2>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ recipesDone }
+      >
+        Receitas Feitas
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ recipesFavorites }
+      >
+        Receitas Favoritas
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ LogoutButton }
+      >
+        Sair
+      </button>
       <Header pageTitle="Perfil" />
       <Footer />
     </div>
   );
 }
-
 export default Profile;
