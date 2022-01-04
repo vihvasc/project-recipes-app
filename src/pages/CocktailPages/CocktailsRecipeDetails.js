262ed26ab17b8fdccedde38b079e6e6baec6b5f9
Huijiro <huijirohankei@gmail.com>
2022-01-04 16:30:18 -0300

    Feito requisitos 33, 34, 35

--- src/pages/CocktailPages/CocktailsRecipeDetails.js
+++ src/pages/CocktailPages/CocktailsRecipeDetails.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Loading from '../components/Loading';
+import filterObjIntoArray from '../helpers/dataManagement';
 
 export default function CockTailsRecipeDetails() {
+  const { id } = useParams();
+  const [cocktail, setCocktail] = useState({});
+  const [ingredients, setIngredients] = useState([]);
+  const [measures, setMeasures] = useState([]);
+  console.log(id);
+
+  const memoizedData = useCallback(
+    async () => {
+      const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const dataDetails = await response.json();
+      setCocktail(dataDetails.drinks[0]);
+      setIngredients(filterObjIntoArray(dataDetails.drinks[0], 'Ingredient'));
+      setMeasures(filterObjIntoArray(dataDetails.drinks[0], 'Measure'));
+    }, [id],
+  );
+
+  useEffect(() => {
+    memoizedData();
+  }, [memoizedData]);
+
   return (
     <div>
-      CockTailsRecipeDetails
+      {cocktail ? (
+        <div>
+          {console.log(cocktail)}
+          <img src={ cocktail.strDrinkThumb } alt="" data-testid="recipe-photo" />
+          <h1 data-testid="recipe-title">{ cocktail.strDrink }</h1>
+          <button type="button" data-testid="share-btn">SHARE</button>
+          <button type="button" data-testid="favorite-btn">FAVORITE</button>
+          <h3 data-testid="recipe-category">{ cocktail.strAlcoholic }</h3>
+
+          <ol>
+            Ingredientes:
+            {ingredients.map((ingredient, index) => (
+              <li
+                key={ index }
+                data-testid={ `${index}-ingredient-name-and-measure` }
+              >
+                {`${ingredient} ${measures[index]}`}
+              </li>
+            ))}
+          </ol>
+          <p data-testid="instructions">
+            Intruções de preparo:
+            <br />
+            {cocktail.strInstructions}
+          </p>
+          <video data-testid="video" controls>
+            <source src={ cocktail.strYoutube } type="video/mp4" />
+            <track src="" kind="captions" srcLang="en" label="English" />
+          </video>
+
+          <button type="button" data-testid="start-recipe-btn">INICIAR RECEITA</button>
+
+          <div>
+            <div data-testid={ `${0}-recomendation-card` }>RECEITA RECOMENDADA 1</div>
+          </div>
+        </div>
+      ) : <Loading />}
     </div>
   );
 }
--- src/pages/FoodPages/FoodRecipeDetails.js
+++ src/pages/FoodPages/FoodRecipeDetails.js
@@ -1,15 +1,69 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+
 import { useParams } from 'react-router-dom';
-import AppContext from '../../context/AppContext';
+import Loading from '../components/Loading';
+import filterObjIntoArray from '../helpers/dataManagement';
 
 export default function FoodRecipeDetails() {
   const { id } = useParams();
+  const [meal, setMeal] = useState({});
+  const [ingredients, setIngredients] = useState([]);
+  const [measures, setMeasures] = useState([]);
   console.log(id);
-  const { data } = useContext(AppContext);
-  console.log('data ao renderizar o :', data);
+
+  const memoizedData = useCallback(
+    async () => {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const dataDetails = await response.json();
+      setMeal(dataDetails.meals[0]);
+      setIngredients(filterObjIntoArray(dataDetails.meals[0], 'Ingredient'));
+      setMeasures(filterObjIntoArray(dataDetails.meals[0], 'Measure'));
+    }, [id],
+  );
+
+  useEffect(() => {
+    memoizedData();
+  }, [memoizedData]);
+
   return (
     <div>
-      FoodRecipeDetails
+      {meal ? (
+        <div>
+          {console.log(meal)}
+          <img src={ meal.strMealThumb } alt="" data-testid="recipe-photo" />
+          <h1 data-testid="recipe-title">{ meal.strMeal }</h1>
+          <button type="button" data-testid="share-btn">SHARE</button>
+          <button type="button" data-testid="favorite-btn">FAVORITE</button>
+          <h3 data-testid="recipe-category">{ meal.strCategory }</h3>
+
+          <ol>
+            Ingredientes:
+            {ingredients.map((ingredient, index) => (
+              <li
+                key={ index }
+                data-testid={ `${index}-ingredient-name-and-measure` }
+              >
+                {`${ingredient} ${measures[index]}`}
+              </li>
+            ))}
+          </ol>
+          <p data-testid="instructions">
+            Intruções de preparo:
+            <br />
+            {meal.strInstructions}
+          </p>
+          <video data-testid="video" controls>
+            <source src={ meal.strYoutube } type="video/mp4" />
+            <track src="" kind="captions" srcLang="en" label="English" />
+          </video>
+
+          <button type="button" data-testid="start-recipe-btn">INICIAR RECEITA</button>
+
+          <div>
+            <div data-testid={ `${0}-recomendation-card` }>RECEITA RECOMENDADA 1</div>
+          </div>
+        </div>
+      ) : <Loading />}
     </div>
   );
 }
