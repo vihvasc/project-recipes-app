435fc401267e7d631b74cb5dc131eeed09c9a125
alvesgf16 <alvesgabriel1994@gmail.com>
2022-01-07 12:47:08 -0300

    Muda estado de favoritas no botão de favoritar

--- src/components/FavoriteButton.js
+++ src/components/FavoriteButton.js
@@ -4,28 +4,27 @@ import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 export default function FavoriteButton({ newFavorite, recipeId }) {
-  const [localFavorites, setLocalFavorites] = useState([]);
+  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
   const [isFavorite, setIsFavorite] = useState(false);
 
   useEffect(() => {
     const storedFavorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
-    console.log(storedFavorites);
-    setLocalFavorites(storedFavorites);
+    setFavoriteRecipes(storedFavorites);
   }, []);
 
   useEffect(() => {
-    setIsFavorite(localFavorites && localFavorites.some(({ id }) => id === recipeId));
-  }, [localFavorites, recipeId]);
+    setIsFavorite(favoriteRecipes && favoriteRecipes.some(({ id }) => id === recipeId));
+  }, [favoriteRecipes, recipeId]);
 
   useEffect(() => {
-    localStorage.setItem('favoriteRecipes', JSON.stringify(localFavorites));
-  }, [localFavorites]);
+    localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipes));
+  }, [favoriteRecipes]);
 
   function handleClick() {
     if (isFavorite) {
-      setLocalFavorites((prevFavs) => prevFavs.filter(({ id }) => id !== recipeId));
+      setFavoriteRecipes((prevFavs) => prevFavs.filter(({ id }) => id !== recipeId));
     } else {
-      setLocalFavorites((prevFavs) => (prevFavs && prevFavs.length > 0
+      setFavoriteRecipes((prevFavs) => (prevFavs && prevFavs.length > 0
         ? [...prevFavs, newFavorite] : [newFavorite]));
     }
   }
