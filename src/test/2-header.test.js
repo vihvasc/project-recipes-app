18fdb7d9dfb1e89d2506d0152ca65f14f58d7853
alvesgf16 <alvesgabriel1994@gmail.com>
2022-01-10 17:42:41 -0300

    Remove arquivos de testes

--- src/test/2-header.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import App from '../App';
-import renderWithRouter from './test-utils';
-
-describe('Testa o Header', () => {
-  it('TEST', () => {
-    renderWithRouter(<App />);
-  });
-});
