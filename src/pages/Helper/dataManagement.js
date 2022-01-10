c3dbd2a5b811c7798833eead7d22636e7c1f4d6c
alvesgf16 <alvesgabriel1994@gmail.com>
2022-01-10 17:51:21 -0300

    Adiciona arquivo faltante

---
+++ src/pages/Helper/dataManagement.js
@@ -0,0 +1,5 @@
+const filterArray = (obj, filter) => Object.values(Object
+  .fromEntries(Object.entries(obj).filter(([key]) => key.includes(filter))))
+  .filter((value) => value);
+
+export default filterArray;
