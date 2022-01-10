71db4b88d6bbe0c38e323db6bd4571bbbdc16ca0
alvesgf16 <alvesgabriel1994@gmail.com>
2022-01-10 16:44:31 -0300

    Implementa tela de explorar por ingredientes

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -8,17 +8,19 @@ import fetchApi from '../services/fetchApi';
 
 function Drinks() {
   const { drinks, setDrinks, data } = useContext(RecipesContext);
-  const { pathname } = useLocation();
+  const { pathname, state } = useLocation();
 
   useEffect(() => {
     async function fetchRecipes() {
-      const apiReturn = await fetchApi('nome', '', pathname);
+      const apiReturn = state
+        ? await fetchApi('ingrediente', state.ingredient, pathname)
+        : await fetchApi('nome', '', pathname);
       const apiReturnArr = Object.values(apiReturn)[0];
       setDrinks(apiReturnArr);
     }
 
     fetchRecipes();
-  }, [pathname, setDrinks]);
+  }, [pathname, setDrinks, state]);
 
   function displayRecipes(recipes) {
     const MAX_RECIPES = 12;
--- src/pages/ExploreDrinkIngredients.js
+++ src/pages/ExploreDrinkIngredients.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function ExploreDrinkIngredients() {
+  const [ingredients, setIngredients] = useState([]);
+
+  async function fetchIngredients() {
+    const REQUEST_LIMIT = 12;
+
+    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list');
+    const data = await response.json();
+    const ingredientsToShow = data.drinks
+      .filter((_ingredient, index) => index < REQUEST_LIMIT);
+    setIngredients(ingredientsToShow);
+  }
+
+  useEffect(() => { fetchIngredients(); }, []);
+
   return (
     <div>
       <Header pageTitle="Explorar Ingredientes" />
+      { ingredients.map(({ strIngredient1: strIngredient }, index) => (
+        <Link
+          key={ `${strIngredient}-card` }
+          data-testid={ `${index}-ingredient-card` }
+          to={ {
+            pathname: '/bebidas',
+            state: { ingredient: `${strIngredient}` },
+          } }
+        >
+          <img
+            src={ `https://www.thecocktaildb.com/images/ingredients/${strIngredient}-Small.png` }
+            alt={ `${strIngredient}` }
+            data-testid={ `${index}-card-img` }
+          />
+          <h4 data-testid={ `${index}-card-name` }>{ strIngredient }</h4>
+        </Link>
+      )) }
       <Footer />
     </div>
   );
