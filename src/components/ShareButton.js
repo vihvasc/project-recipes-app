91225d1b7f2d19de966a313598e12b28196c6a7d
alvesgf16 <alvesgabriel1994@gmail.com>
2022-01-10 09:01:00 -0300

    Corrige divergência de case do data-testid

--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -5,7 +5,7 @@ import shareIcon from '../images/shareIcon.svg';
 
 const copy = require('clipboard-copy');
 
-export default function ShareButton({ pathname, dataTestId }) {
+export default function ShareButton({ pathname, dataTestid }) {
   const [isCopied, setIsCopied] = useState(false);
 
   function handleClick() {
@@ -19,7 +19,7 @@ export default function ShareButton({ pathname, dataTestId }) {
         type="button"
         onClick={ handleClick }
       >
-        <img data-testid={ dataTestId } src={ shareIcon } alt="Profile-icon" />
+        <img data-testid={ dataTestid } src={ shareIcon } alt="Profile-icon" />
       </button>
       { isCopied && (
         <Alert variant="success">
@@ -32,5 +32,5 @@ export default function ShareButton({ pathname, dataTestId }) {
 
 ShareButton.propTypes = {
   pathname: PropTypes.string.isRequired,
-  dataTestId: PropTypes.string.isRequired,
+  dataTestid: PropTypes.string.isRequired,
 };
